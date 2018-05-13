27d1850cb7e4421afc131fa903191700f190c3f5
adityab03 <35015490+adityab03@users.noreply.github.com>
2018-05-13 19:49:27 +0700

    Add files via upload

---
+++ sudit/admin/jumbotron.php
@@ -0,0 +1,16 @@
+
+<body>
+<div class="container">
+<div class="jumbotron" style="background-color: rgba(255,255,255,0.60);">
+
+
+		<p><h1>Selamat Datang di Sudit</h1></p>
+		
+		<p><div class="glyphicon glyphicon-book" style="font-size: 42px;"></div></p>
+		<p><h4>Sistem ini merupakan sebuah halaman yang membantu dalam manajemen perpustakaan.</h4></p>
+		<p><h4>Dibuat dengan Kesederhanaan dan Penuh Semangat '45</h4></p>
+		<p><a class="btn btn-primary btn-lg" role="button" href="?page=buku">Lanjutkan . . .</a></p>
+	</div>
+</div>
+</body>
+</html>
